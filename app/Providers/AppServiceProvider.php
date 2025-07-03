61948db8e2c853418582bf89e7bf2fd4be9cf83f
CHARLIE ETIENNE <ce@web-nancy.fr>
2025-07-03 12:02:57 +0200

    feat: add some Laravel goodies on boot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,13 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Validation\Rules\Password;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +25,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        DB::prohibitDestructiveCommands(app()->isProduction());
+        URL::forceScheme('https');
+        Vite::useAggressivePrefetching();
+        Model::unguard();
+        Model::automaticallyEagerLoadRelationships();
+        Carbon::setLocale(config('app.locale'));
+        Password::defaults(function () {
+            return Password::min(8)
+                ->mixedCase()
+                ->numbers()
+                ->uncompromised();
+        });
     }
 }
