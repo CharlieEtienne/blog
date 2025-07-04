18738608d50780bbed52c703097c3f7e4bcc5ba7
CHARLIE ETIENNE <ce@web-nancy.fr>
2025-07-04 12:26:27 +0200

    feat: add routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,32 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TagController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\CategoryController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', HomeController::class)
+    ->name('home');
+
+Route::get('/blog', [PostController::class, 'index'])
+    ->name('posts.index');
+
+Route::get('/categories', [CategoryController::class, 'index'])
+    ->name('categories.index');
+
+Route::get('/categories/{category:slug}', [CategoryController::class, 'show'])
+    ->name('categories.show');
+
+Route::get('/tags', [TagController::class, 'index'])
+    ->name('tags.index');
+
+Route::get('/tags/{tag:slug}', [TagController::class, 'show'])
+    ->name('tags.show');
+
+Route::get('/{post:slug}', [PostController::class, 'show'])
+    ->name('posts.show');
