a4253ad8bef38f108d6a382a15bf58d9fdc7b4d0
CHARLIE ETIENNE <ce@web-nancy.fr>
2025-07-06 16:10:35 +0200

    feat: Add install command and update the readme accordingly

--- README.md
+++ README.md
@@ -1,13 +1,106 @@
-# charlieetienne.com website
+# TALL Stack Blog App
 
-This repo contains the [charlieetienne.com](https://charlieetienne.com) website code.
+A fully-featured, configurable blog.
 
-It's fully open source!
+Clone the project, log in as admin, tweak some settings and push to prod ☁️.
 
-It's written with:
+It's fully open source. 
 
-- Laravel
+## Stack
+
+- Laravel v12
 - Livewire
 - AlpineJS
-- TailwindCSS
-- Filament
+- TailwindCSS v4
+- Filament v4
+
+## Features
+
+### Full Admin Panel (Filament) with:
+  - [x] Blog posts
+  - [x] Categories
+  - [x] Tags
+  - [ ] Comments
+  - [ ] Multilingual
+  - [ ] CMS Pages
+  - [ ] Portfolio
+  - [ ] Menu Builder
+  - [ ] Analytics with panphp/pan
+
+### Front end (TALL Stack), with:
+  - [x] Homepage
+  - [x] About Section
+  - [x] Blog
+  - [x] Categories
+  - [x] Tags
+
+### You'll be able to configure a lot of options from the Settings page:
+  - [x] Post default image
+  - [x] Site logo
+  - [x] Favicon
+  - [x] Site name
+  - [x] Display site name
+  - [x] Primary color
+  - [x] Heading font
+  - [x] Body font
+  - [x] Code font
+  - [x] Code theme
+  - [x] Hero title
+  - [x] Hero subtitle
+  - [x] Hero image
+  - [x] Hero image height
+  - [x] Hero image full width
+  - [x] About image
+  - [x] About text
+  - [x] About title
+  - [x] About image circular
+  - [x] About image width
+  - [x] About image height
+  - [x] Contact email
+  - [x] Footer text
+  - [x] Copyright text
+  - ...
+
+### Misc
+
+Fonts are loaded through [fonts.bunny.net](https://fonts.bunny.net/) so your blog is GDPR friendly.
+
+## Installation
+
+### 1. Clone the repo
+
+```bash
+git clone https://github.com/charlieetienne/blog.git
+```
+
+### 2. Install dependencies
+
+```bash
+composer install
+```
+
+### 3. Run the installer
+
+You will be prompt for several configuration options.
+
+```bash
+php artisan blog:install
+```
+
+### 4. Enjoy your new blog!
+
+## Testing
+
+You can run the test suite with the following command:
+
+```bash
+php artisan test --parallel
+```
+
+## Contribute
+
+Contributions are welcome!
+
+## Credits
+
+Some parts of this blog are heavily inspired by https://github.com/benjamincrozat/blog-v5
---
+++ app/Console/Commands/InstallCommand.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+use Symfony\Component\Process\Process;
+
+class InstallCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:install';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Install and configure the blog application';
+
+    /**
+     * Execute the console command.
+     *
+     * @throws \Throwable
+     */
+    public function handle(): void
+    {
+        $this->info('Installing your Blog...');
+
+        // Step 1: Copy .env file if it doesn't exist
+        $this->info('Step 1: Creating the environment file...');
+        if (!File::exists('.env')) {
+            File::copy('.env.example', '.env');
+            $this->info('Environment file created.');
+        }
+        else {
+            $this->warn('Environment file already exists.');
+        }
+
+        $appName = $this->ask('What is the name of your application?', 'My Blog');
+        $envContent = File::get('.env');
+        $envContent = preg_replace('/^APP_NAME=.*$/m', 'APP_NAME="' . $appName . '"', $envContent);
+        File::put('.env', $envContent);
+        $this->info('Application name set to: ' . $appName);
+
+        // Step 2: Generate the application key
+        $this->info('Step 2: Generating the application key...');
+        $this->call('key:generate', ['--ansi' => true]);
+
+        // Step 3: Creating the SQLite database
+        $this->info('Step 3: Creating the SQLite database...');
+        if (!File::exists(database_path('database.sqlite'))) {
+            File::put(database_path('database.sqlite'), '');
+            $this->info('SQLite database created.');
+        }
+        else {
+            $this->fail('SQLite database already exists.');
+        }
+
+        // Step 4: Run migrations
+        $this->info('Step 4: Running migrations...');
+        $seedData = $this->confirm('Would you like to seed some dummy data?', false);
+        $this->call('migrate', $seedData ? ['--seed' => true] : []);
+
+        // Step 5: Install NPM dependencies and build assets
+        $this->info('Step 5: Installing NPM dependencies...');
+        $process = Process::fromShellCommandline('npm install && npm run build');
+        $process->setTimeout(null);
+        $process->run(function ($type, $line) {
+            $this->output->write($line);
+        });
+
+        // Step 6: Create Admin User
+        $this->info('Step 6: Creating Admin User...');
+        $this->call('make:filament-user');
+
+        $this->info('Blog installation complete! You can now start using your blog.');
+    }
+}
