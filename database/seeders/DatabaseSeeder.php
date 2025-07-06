0a74c9214f634d0624b639b8b8053b5666af5d9b
CHARLIE ETIENNE <ce@web-nancy.fr>
2025-07-06 21:02:37 +0200

    fix: replace personal info with dummy data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,8 +17,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $admin = User::factory()->create([
-            'name' => 'Charlie Etienne',
-            'email' => 'hello@charlieetienne.com',
+            'name' => 'Admin',
+            'email' => 'hello@example.com',
         ]);
 
         $users = User::factory(10)->create();
